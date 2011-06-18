d537f093bd0429206b5fcd0181dc605b4589fcb6
Muharem Hrnjadovic <mh@foldr3.com>
2011-06-18 12:01:53 +0200

    refactored control script so it works with all producers/consumers

--- control.py
+++ control.py
@@ -1,31 +1,43 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # vim: tabstop=4 shiftwidth=4 softtabstop=4
 
 
 """
-Control the twitter pump.
+Control the various amqp producers and consumers.
 """
 
 
+from optparse import OptionParser
+
 import kombu
 
+from utils import config
 
-def main(config):
-    connection = kombu.BrokerConnection(**config)
+
+def main(options):
+    connection = kombu.BrokerConnection(**config.get_rabbitmq_config())
     channel = connection.channel()
-    # By default messages sent to exchanges are persistent (delivery_mode=2),
+    # By default messages sent to exchanges are persistent,
     # and queues and exchanges are durable.
-    control_queue = connection.SimpleQueue("twitter_pump")
+    control_queue = connection.SimpleQueue(options.queue,
+                                           queue_opts=dict(durable=True))
     try:
         while True:
-            cmd = raw_input("quit|question for the twitter pump: ")
-            control_queue.put(cmd.strip())
+            cmd = raw_input(options.prompt)
+            control_queue.put(
+                cmd.strip(),
+                delivery_mode=kombu.entity.TRANSIENT_DELIVERY_MODE)
     finally:
         channel.close()
         connection.close()
 
 
 if __name__ == '__main__':
-    config = dict(hostname="localhost", userid="guest", password="guest",
-                  virtual_host="/")
-    main(config)
+    parser = OptionParser(usage="usage: %prog [options]")
+    parser.add_option("-p", "--prompt", dest="prompt",
+                      help="the prompt to display to user")
+    parser.add_option("-q", "--queue", dest="queue",
+                      help="the name of the control queue")
+    (options, args) = parser.parse_args()
+    main(options)
