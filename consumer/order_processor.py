5d8f019d72579047e4f0b1b91501432f53d62225
Muharem Hrnjadovic <mh@foldr3.com>
2011-06-20 18:33:36 +0200

    split order processing code

--- consumer/order_processor.py
+++ consumer/order_processor.py
@@ -9,14 +9,11 @@ Simple order processor server, adapted from
 """
 
 
-import simplejson
-
 import pika
-
 from utils import config
 
 
-channel = None
+__channel = None
 
 
 def on_connected(connection):
@@ -27,32 +24,14 @@ def on_connected(connection):
 
 def on_channel_open(new_channel):
     """Called when our channel has opened"""
-    global channel
-    channel = new_channel
+    global __channel
+    __channel = new_channel
 
-    # Preprocessing exchange + queues for non-validated orders.
-    channel.exchange_declare(exchange="incoming", durable=False, type="direct",
-                             auto_delete=True)
-    channel.queue_declare(queue="incoming", durable=False, auto_delete=True,
-                          callback=on_preproc_queue_declared)
-    channel.queue_declare(queue="decrypted", durable=False, auto_delete=True,
-                          callback=on_preproc_queue_declared)
     # Fanout exchange + queues for validated orders.
-    channel.exchange_declare(exchange="orders", durable=False, type="fanout",
+    __channel.exchange_declare(exchange="orders", durable=False, type="fanout",
                              auto_delete=True)
-    channel.queue_declare(queue="validated", durable=False,
+    __channel.queue_declare(queue="validated", durable=False,
                           auto_delete=True, callback=on_orders_queue_declared)
-
-
-def on_preproc_queue_declared(frame):
-    """Called when a queue has been declared on the `incoming` exchange."""
-    callbacks = dict(incoming=decrypt, decrypted=authenticate)
-    queue_name  = frame.method.queue
-    assert queue_name in callbacks, "Unknown queue: %s" % queue_name
-
-    channel.basic_consume(callbacks[queue_name], queue=queue_name)
-    channel.queue_bind(exchange="incoming", queue=queue_name,
-                       routing_key="rk-%s" % queue_name)
 
 
 def on_orders_queue_declared(frame):
@@ -61,47 +40,14 @@ def on_orders_queue_declared(frame):
     queue_name  = frame.method.queue
     assert queue_name in callbacks, "Unknown queue: %s" % queue_name
 
-    channel.basic_consume(callbacks[queue_name], queue=queue_name)
-    channel.queue_bind(exchange="orders", queue=queue_name)
-
-
-def decrypt(channel, method, header, body):
-    """Called when we receive an encrypted order."""
-    print "\n* Encrypted order: %s" % body
-    body = body.decode("rot13")
-    if body == "quit":
-        connection.close()
-        connection.ioloop.start()
-        return
-
-    channel.basic_ack(delivery_tag=method.delivery_tag)
-    order = simplejson.loads(body)
-    assert isinstance(order, list) and len(order) == 2, \
-        "Malformed order %s" % order
-    channel.basic_publish(exchange="incoming", routing_key="rk-decrypted",
-        body=body, properties=pika.BasicProperties(delivery_mode=1))
-    print "> Decrypted: %s" % body
-
-
-def authenticate(channel, method, header, body):
-    """Called with a decrypted order."""
-    print "* Decrypted: %s" % body
-    channel.basic_ack(delivery_tag=method.delivery_tag)
-
-    customer, order = simplejson.loads(body)
-    if customer != "al_maisan":
-        print "! Unknown customer: %s" % customer
-    else:
-        channel.basic_publish(exchange="orders", routing_key="validated",
-                              body=order)
-        print "> Authenticated: %s" % order
+    __channel.basic_consume(callbacks[queue_name], queue=queue_name)
+    __channel.queue_bind(exchange="orders", queue=queue_name)
 
 
 def process(channel, method, header, body):
     """Called with a validated order."""
-    print "* Validated: %s" % body
+    print "|R| orders.validated: %s" % body
     channel.basic_ack(delivery_tag=method.delivery_tag)
-    print "> Processed: %s" % body
 
 
 connection = pika.adapters.SelectConnection(config.pika_params(), on_connected)
---
+++ consumer/order_validator.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+
+"""
+Simple order validation server, adapted from
+    http://pika.github.com/connecting.html#continuation-passing-style
+"""
+
+
+import simplejson
+import pika
+from utils import config
+
+
+__channel = None
+
+
+def on_connected(connection):
+    """Called when we are fully connected to RabbitMQ"""
+    # Open a channel
+    connection.channel(on_channel_open)
+
+
+def on_channel_open(new_channel):
+    """Called when our channel has opened"""
+    global __channel
+    __channel = new_channel
+
+    # Preprocessing exchange + queues for non-validated orders.
+    __channel.exchange_declare(exchange="incoming", durable=False,
+                               type="direct", auto_delete=True)
+    __channel.queue_declare(queue="incoming", durable=False, auto_delete=True,
+                          callback=on_incoming_queue_declared)
+    __channel.queue_declare(queue="decrypted", durable=False, auto_delete=True,
+                          callback=on_incoming_queue_declared)
+    __channel.exchange_declare(exchange="orders", durable=False, type="fanout",
+                             auto_delete=True)
+
+
+def on_incoming_queue_declared(frame):
+    """Called when a queue has been declared on the `incoming` exchange."""
+    callbacks = dict(incoming=decrypt, decrypted=authenticate)
+    queue_name  = frame.method.queue
+    assert queue_name in callbacks, "Unknown queue: %s" % queue_name
+
+    __channel.basic_consume(callbacks[queue_name], queue=queue_name)
+    __channel.queue_bind(exchange="incoming", queue=queue_name,
+                       routing_key="rk-%s" % queue_name)
+
+
+def decrypt(channel, method, header, body):
+    """Called when we receive an encrypted order."""
+    print "\n|R| incoming.incoming: %s" % body
+    body = body.decode("rot13")
+    if body == "quit":
+        connection.close()
+        connection.ioloop.start()
+        return
+
+    channel.basic_ack(delivery_tag=method.delivery_tag)
+    order = simplejson.loads(body)
+    assert isinstance(order, list) and len(order) == 2, \
+        "Malformed order %s" % order
+    channel.basic_publish(exchange="incoming", routing_key="rk-decrypted",
+        body=body, properties=pika.BasicProperties(delivery_mode=1))
+    print "|W| incoming.decrypted: %s" % body
+
+
+def authenticate(channel, method, header, body):
+    """Called with a decrypted order."""
+    print "|R| incoming.decrypted: %s" % body
+    channel.basic_ack(delivery_tag=method.delivery_tag)
+
+    customer, order = simplejson.loads(body)
+    if customer != "al_maisan":
+        print "!!! Unknown customer: %s" % customer
+    else:
+        channel.basic_publish(exchange="orders", body=order, routing_key="")
+        print "|W| orders._: %s" % order
+
+
+connection = pika.adapters.SelectConnection(config.pika_params(), on_connected)
+
+
+try:
+    # Loop so we can communicate with RabbitMQ
+    connection.ioloop.start()
+finally:
+    # Gracefully close the connection
+    connection.close()
+    # Loop until we're fully closed, will stop on its own
+    connection.ioloop.start()
